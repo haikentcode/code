caf57c398fee28fd00562c1804518011a7e49336
haikentcode <hiteshnitj16@gmail.com>
2015-09-27 07:59:56 +0530

    getchinlinux

--- pythonOpencv/imageLoader.py
+++ pythonOpencv/imageLoader.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
-img=cv2.imread('demo.jpg',1)
-cv2.imshow('image',img)
+img=cv2.imread('data/demo.jpg',1)
+cv2.imshow('data/image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
