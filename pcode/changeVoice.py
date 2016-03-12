8d5017b23953f9b650bd3aca14d639a105117944
haikentcode <hiteshnitj16@gmail.com>
2016-03-13 02:05:57 +0530

    clean

---
+++ pcode/changeVoice.py
@@ -0,0 +1,8 @@
+import pyttsx
+engine = pyttsx.init()
+voices = engine.getProperty('voices')
+for voice in voices:
+   engine.setProperty('voice', voice.id)
+   engine.say('The quick brown fox jumped over the lazy dog.')
+engine.runAndWait()
+
